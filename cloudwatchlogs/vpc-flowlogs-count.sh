8deb2a2f2702477736680ec20de045975ef95538
Gian Carlo Val Ebao <gianebao@gmail.com>
2017-08-15 13:26:20 +0800

    fixed millisecond

--- cloudwatchlogs/vpc-flowlogs-count.sh
+++ cloudwatchlogs/vpc-flowlogs-count.sh
@@ -10,10 +10,10 @@ function main() {
  foreach ($d as $i) echo implode(",", [
   $i->logStreamName,
   $i->storedBytes,
-  date("Y-m-d H:i:s", $i->lastIngestionTime),
-  date("Y-m-d H:i:s", $i->lastEventTimestamp),
-  date("Y-m-d H:i:s", $i->firstEventTimestamp),
-  date("Y-m-d H:i:s", $i->creationTime)]);'
+  date("Y-m-d H:i:s", $i->lastIngestionTime/1000),
+  date("Y-m-d H:i:s", $i->lastEventTimestamp/1000),
+  date("Y-m-d H:i:s", $i->firstEventTimestamp/1000),
+  date("Y-m-d H:i:s", $i->creationTime/1000)]);'
 
 
   echo "logStreamName,storedBytes,lastIngestionTime,lastEventTimestamp,firstEventTimestamp,creationTime"
