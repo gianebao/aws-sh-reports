8655e59b58de1054f473c86aa00e13a5f2bec5a6
Gian Carlo Val Ebao <gianebao@gmail.com>
2017-08-15 13:13:27 +0800

    added flow log report

---
+++ cloudwatchlogs/vpc-flowlogs-count.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+PHPD='$d = file_get_contents("php://stdin")'
+JSONDECODE='$d = json_decode('"$PHPD"');'
+
+# start-time end-time period statistics
+function main() {
+  m=`aws logs describe-log-streams --log-group-name $1`
+  p='$d = $d->logStreams;
+ foreach ($d as $i) echo implode(",", [
+  $i->logStreamName,
+  $i->storedBytes,
+  date("Y-m-d H:i:s", $i->lastIngestionTime),
+  date("Y-m-d H:i:s", $i->lastEventTimestamp),
+  date("Y-m-d H:i:s", $i->firstEventTimestamp),
+  date("Y-m-d H:i:s", $i->creationTime)]);'
+
+
+  echo "logStreamName,storedBytes,lastIngestionTime,lastEventTimestamp,firstEventTimestamp,creationTime"
+  echo "$m" | php -r "$JSONDECODE""$p"
+}
+
+main "$1"
