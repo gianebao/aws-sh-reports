907ebb15ec6054e171ceeaa62b4e6880ca980dd0
Gian Carlo Val Ebao <gianebao@gmail.com>
2017-08-15 13:28:40 +0800

    added new line

--- cloudwatchlogs/vpc-flowlogs-count.sh
+++ cloudwatchlogs/vpc-flowlogs-count.sh
@@ -13,7 +13,8 @@ function main() {
   date("Y-m-d H:i:s", $i->lastIngestionTime/1000),
   date("Y-m-d H:i:s", $i->lastEventTimestamp/1000),
   date("Y-m-d H:i:s", $i->firstEventTimestamp/1000),
-  date("Y-m-d H:i:s", $i->creationTime/1000)]);'
+  date("Y-m-d H:i:s", $i->creationTime/1000),
+  "\n"]);'
 
 
   echo "logStreamName,storedBytes,lastIngestionTime,lastEventTimestamp,firstEventTimestamp,creationTime"
